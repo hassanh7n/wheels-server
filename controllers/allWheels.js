311c59eccb3359ff1aff0d4f3e2f9ecb670d96b9
unknown <hassanwebdeveloper8322@gmail.com>
2024-08-01 01:48:00 +0500

    updating some things

--- controllers/allWheels.js
+++ controllers/allWheels.js
@@ -71,7 +71,7 @@ const getAllWheels = async(req, res) => {
     // pagination
 
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 8;
     
     const skip = (page - 1) * limit;
 
